0e206b44f5f10214dc80a7c9ac2d0bbea05d4c71
Vincent Wang <vw12@rice.edu>
2024-06-08 16:51:00 -0700

    add 3d derivative matrices

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,14 @@
 using BernsteinBasis
 using Test
 
-@testset "BernsteinBasis.jl" begin
-    # Write your tests here.
+@testset "3D derivative matrix fast multiply" begin
+    for N in 1:12
+        Np = div((N + 1) * (N + 2) * (N + 3), 6)
+        x = rand(Float64, Np)
+        out = zeros(Float64, Np)
+        # Converts LHS to Matrix so it doesn't use mul!.
+        @test collect(BernsteinDerivativeMatrix_3D_r(N)) * x ≈ mul!(out, BernsteinDerivativeMatrix_3D_r(N), x)
+        @test collect(BernsteinDerivativeMatrix_3D_s(N)) * x ≈ mul!(out, BernsteinDerivativeMatrix_3D_s(N), x)
+        @test collect(BernsteinDerivativeMatrix_3D_t(N)) * x ≈ mul!(out, BernsteinDerivativeMatrix_3D_t(N), x)
+    end
 end
