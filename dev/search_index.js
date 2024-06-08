8729327cd6c94d269ce28516fd1653427ba10ffb
Documenter.jl <documenter@juliadocs.github.io>
2024-06-08 09:43:52 +0000

    build based on 33bc84d

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = BernsteinBasis","category":"page"},{"location":"#BernsteinBasis","page":"Home","title":"BernsteinBasis","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for BernsteinBasis.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [BernsteinBasis]","category":"page"}]
+}
