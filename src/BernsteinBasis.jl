e2598198d1a25e6cef06ac976c4eb1e1af21935e
Vincent Wang <vw12@rice.edu>
2024-06-10 22:17:04 -0700

    export BernsteinLift

--- src/BernsteinBasis.jl
+++ src/BernsteinBasis.jl
@@ -3,8 +3,11 @@ module BernsteinBasis
 using LinearAlgebra
 
 include("BernsteinDerivativeMatrix_3D.jl")
+include("BernsteinLift.jl")
 # Write your package code here.
 export BernsteinDerivativeMatrix_3D_r, BernsteinDerivativeMatrix_3D_s, 
     BernsteinDerivativeMatrix_3D_t, mul!
     
+export BernsteinLift, mul!
+
 end
