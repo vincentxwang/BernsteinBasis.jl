e8c9e5d3e6541e03edf3d8df99c52b233ec4f5af
Vincent Wang <vw12@rice.edu>
2024-06-19 18:18:00 -0700

    plot functions

--- test/performance.jl
+++ test/performance.jl
@@ -2,19 +2,101 @@ using BenchmarkTools
 using BernsteinBasis
 using LinearAlgebra
 
+N = 8
+Np2 = div((N + 1) * (N + 2), 2)
+Np3 = div((N + 1) * (N + 2) * (N + 3), 6)
 
-println("Lift matrix-vector multiplication for N = 7")
-@btime mul!($(zeros(120)), $(BernsteinLift(7)), $(rand(Float64, 36)))
-@btime mul!($(zeros(120)), $(rand(Float64, 120, 36)), $(rand(Float64, 36)))
+println("Lift matrix-vector multiplication for N = ", N)
+@btime mul!($(zeros(Np3)), $(BernsteinLift(N)), $(rand(Float64, Np2)))
+@btime mul!($(zeros(Np3)), $(rand(Float64, Np3, Np2)), $(rand(Float64, Np2)))
 
-println("Lift matrix-matrix multiplication for N = 7")
-@btime mul!($(zeros(120, 120)), $(BernsteinLift(7)), $(rand(Float64, 36, 120))) 
-@btime mul!($(zeros(120, 120)), $(rand(Float64, 120, 36)), $(rand(Float64, 36, 120))) 
+println("Lift matrix-matrix multiplication for N = ", N)
+@btime mul!($(zeros(Float64, Np3, Np3)), $(BernsteinLift(N)), $(rand(Float64, Np2, Np3))) 
+@btime mul!($(zeros(Np3, Np3)), $(rand(Float64, Np3, Np2)), $(rand(Float64, Np2, Np3))) 
 
-println("Derivative matrix-matrix multiplication for N = 7")
-@btime mul!($(zeros(120, 120)), $(BernsteinDerivativeMatrix_3D_r(7)), $(rand(Float64, 120, 120))) 
-@btime mul!($(zeros(120, 120)), $(Matrix(BernsteinDerivativeMatrix_3D_r(7))), $(rand(Float64, 120, 120))) 
+println("Derivative matrix-matrix multiplication for N = ", N)
+@btime mul!($(zeros(Np3, Np3)), $(BernsteinDerivativeMatrix_3D_r(N)), $(rand(Float64, Np3, Np3))) 
+@btime mul!($(zeros(Np3, Np3)), $(Matrix(BernsteinDerivativeMatrix_3D_r(N))), $(rand(Float64, Np3, Np3))) 
 
-println("Derivative matrix-vector multiplication for N = 7")
-@btime mul!($(zeros(120)), $(BernsteinDerivativeMatrix_3D_r(7)), $(rand(Float64, 120))) 
-@btime mul!($(zeros(120)), $(Matrix(BernsteinDerivativeMatrix_3D_r(7))), $(rand(Float64, 120))) 
+println("Derivative matrix-vector multiplication for N = ", N)
+@btime mul!($(zeros(Np3)), $(BernsteinDerivativeMatrix_3D_r(N)), $(rand(Float64, Np3))) 
+@btime mul!($(zeros(Np3)), $(Matrix(BernsteinDerivativeMatrix_3D_r(N))), $(rand(Float64, Np3))) 
+
+# Naive matrix multiplication for benchmarking
+function my_mul!(C,A,B)
+    n,m = size(A)
+    @inbounds @simd for i in 1:n
+        for j in 1:m 
+            for k in 1:m
+                C[i,j] += A[i,k]*B[k,j]
+            end
+        end
+    end
+end
+
+using StatsPlots
+function make_lift_plot(K)
+
+    bernstein_times = Float64[]
+    opt_dense_times = Float64[]
+    unopt_dense_times = Float64[]
+
+    for N in 1:K
+        Np2 = div((N + 1) * (N + 2), 2)
+        Np3 = div((N + 1) * (N + 2) * (N + 3), 6)
+
+        time1 = @benchmark mul!($(zeros(Float64, Np3, Np3)), $(BernsteinLift(N)), $(rand(Float64, Np2, Np3))) seconds=1
+        push!(bernstein_times, median(time1).time) 
+
+        time2 = @benchmark mul!($(zeros(Float64, Np3, Np3)), $(rand(Float64, Np3, Np2)), $(rand(Float64, Np2, Np3))) seconds=1
+        push!(opt_dense_times, median(time2).time) 
+
+        time3 = @benchmark my_mul!($(zeros(Float64, Np3, Np3)), $(rand(Float64, Np3, Np2)), $(rand(Float64, Np2, Np3))) seconds=1
+        push!(unopt_dense_times, median(time3).time) 
+    end
+
+    times = vcat(bernstein_times, opt_dense_times, unopt_dense_times)
+    
+    ctg = repeat(["Bernstein", "Dense opt", "Dense unopt"], inner = K)
+    nam = repeat(string.(1:K), outer = 3)
+
+    groupedbar(nam, times, group = ctg, xlabel = "N", ylabel = "Time",
+            title = "Lift matrix-matrix multiplication, median times", bar_width = 0.67,
+            lw = 0, framestyle = :box)
+end
+
+function make_der_plot(K)
+
+    bernstein_times = Float64[]
+    opt_dense_times = Float64[]
+    # unopt_dense_times = Float64[]
+
+    for N in 1:K
+        Np2 = div((N + 1) * (N + 2), 2)
+        Np3 = div((N + 1) * (N + 2) * (N + 3), 6)
+
+        time1 = @benchmark mul!($(zeros(Float64, Np3, Np3)), $(BernsteinDerivativeMatrix_3D_r(N)), $(rand(Float64, Np3, Np3))) seconds=1
+        push!(bernstein_times, median(time1).time) 
+
+        time2 = @benchmark mul!($(zeros(Float64, Np3, Np3)), $(Matrix(BernsteinDerivativeMatrix_3D_r(N))), $(rand(Float64, Np3, Np3))) seconds=1
+        push!(opt_dense_times, median(time2).time) 
+
+        # time3 = @benchmark my_mul!($(zeros(Float64, Np3, Np3)), $(Matrix(BernsteinDerivativeMatrix_3D_r(N))), $(rand(Float64, Np3, Np3))) seconds=1
+        # push!(unopt_dense_times, median(time3).time) 
+    end
+
+    times = vcat(bernstein_times, opt_dense_times)
+    
+    ctg = repeat(["Bernstein", "Dense opt"], inner = K)
+    nam = repeat(string.(1:K), outer = 2)
+
+    groupedbar(nam, times, group = ctg, xlabel = "N", ylabel = "Time",
+            title = "Derivative matrix-matrix multiplication, median times", bar_width = 0.67,
+            lw = 0, framestyle = :box)
+end
+
+# make_lift_plot(6)
+# make_der_plot(6)
+
+
+
